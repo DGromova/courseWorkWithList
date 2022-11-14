37e6b656a5fc943e1b7e584435d3b339e06d0758
Daria Gromova <igromova81@gmail.com>
2022-11-14 20:10:10 +0300

    Initial commit

---
+++ src/main/java/com/example/courseworkwithlist/Employee.java
@@ -0,0 +1,2 @@
+package com.example.courseworkwithlist;public class Employye {
+}
---
+++ src/test/java/com/example/courseworkwithlist/CourseWorkWithListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworkwithlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkWithListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
